6baadc5e0c2ff91eb74c26c1e1a055b445c254e5
Daniel Dipas <danieldipas11@hotmail.com>
2020-07-30 15:33:26 +0200

    Correccion pom.xml Primer Ejercicio

---
+++ src/main/java/dam107ABDe1/Main.java
@@ -0,0 +1,30 @@
+/*
+Realizar un programa que se conecte a la base de datos ‘empresa’ y muestre el nombre de los
+empleados de la empresa
+ */
+package dam107ABDe1;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            DAOEmpresa dao = DAOEmpresa.getInstance();
+            ArrayList<Empleado> emps = dao.RecogeEmpleados();
+            for(Empleado x : emps){
+                System.out.println("-------------------------");
+                System.out.println("ID: "  + x.getId());
+                System.out.println("Nombre: "  + x.getNombre());
+                System.out.println("-------------------------");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
