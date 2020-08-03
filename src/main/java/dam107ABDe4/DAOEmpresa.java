4663da4e2b7031572b6e6576e428241d2cfac4d3
Daniel Dipas <danieldipas11@hotmail.com>
2020-08-03 13:23:54 +0200

    Ejercicio 4

---
+++ src/main/java/dam107ABDe4/DAOEmpresa.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam107ABDe4;
+
+import dam107ABDe3.*;
+import dam107ABDe2.*;
+import dam107ABDe1.*;
+import java.io.IOException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author User
+ */
+public class DAOEmpresa {
+
+    private static DAOEmpresa instance = null;
+    private Connection con = null;
+
+    private DAOEmpresa() throws SQLException, IOException {
+        if (con == null) {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/empresa", "root", "");
+        }
+    }
+
+    public static DAOEmpresa getInstance() throws SQLException, IOException {
+        if (instance == null) {
+            instance = new DAOEmpresa();
+        }
+        return instance;
+    }
+    
+    public ArrayList<Empleado> RecogeEmpleados() throws SQLException{
+        ArrayList <Empleado> emps = new ArrayList<>();
+        String consulta = "SELECT id, nombre, salario, fechaNacimiento, categoria FROM empleado";
+        PreparedStatement ps = con.prepareStatement(consulta);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                String nombre = rs.getString("nombre");
+                int id = rs.getInt("id");
+                float salario = rs.getFloat("salario");
+                LocalDate fechaNac= rs.getDate("fechaNacimiento").toLocalDate();
+                String categoria = rs.getString("categoria");
+                emps.add(new Empleado(id, nombre, salario, fechaNac, categoria));
+            }
+        return emps;
+    }
+    
+}
---
+++ src/main/java/dam107ABDe4/Main.java
@@ -0,0 +1,31 @@
+/*
+Crear una clase llamada Empleado cuyos atributos son los campos de la tabla ‘Empleado’.
+Realizar un programa que cargue un ArrayList de clase Empleado con todos los empleados de
+la tabla. Mostrar a continuación todo el contenido del ArrayList. Pista: Implementa el método
+toString() en la clase Empleado 
+ */
+package dam107ABDe4;
+
+import java.io.IOException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        
+        try {
+            DAOEmpresa dao = DAOEmpresa.getInstance();
+            ArrayList <Empleado> emps = dao.RecogeEmpleados();
+            for(Empleado x : emps)
+                System.out.println(x.toString());
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
