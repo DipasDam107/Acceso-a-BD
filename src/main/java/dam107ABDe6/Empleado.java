beabf4d59b0caaf2c5b8f5695f2afc0b39bcc65c
Daniel Dipas <danieldipas11@hotmail.com>
2020-08-03 14:11:58 +0200

    Ejercicio 6

---
+++ src/main/java/dam107ABDe6/Empleado.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam107ABDe6;
+
+import dam107ABDe5.*;
+import dam107ABDe4.*;
+import dam107ABDe3.*;
+import dam107ABDe2.*;
+import dam107ABDe1.*;
+import java.time.LocalDate;
+
+public class Empleado {
+    private int id;
+    private String nombre;
+    private float salario;
+    private LocalDate fechaNac;
+    private String categoria;
+
+    public Empleado(int id, String nombre, float salario, LocalDate fechaNac, String categoria) {
+        this.id = id;
+        this.nombre = nombre;
+        this.salario = salario;
+        this.fechaNac = fechaNac;
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public LocalDate getFechaNac() {
+        return fechaNac;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado: "+this.id+ " Nombre: " + this.nombre + " Categoria: " + this.categoria;
+    }
+    
+
+   
+    
+    
+}
---
+++ src/main/java/dam107ABDe6/Main.java
@@ -0,0 +1,60 @@
+/*
+Realizar una versión del programa anterior en la que el usuario no introduce el ‘id’. El sistema
+la calcula como el máximo almacenado en la tabla + 1. Pista: Habrá dos PreparedStatemnt,
+uno para obtener el max (i) y otro para el insert
+ */
+package dam107ABDe6;
+
+import java.io.IOException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main {
+    static Scanner teclado;
+    public static void main(String[] args) {
+        teclado = new Scanner(System.in);
+        try {
+            DAOEmpresa dao = DAOEmpresa.getInstance();
+            int opcion=0;
+            do{
+                int idMax = dao.getMaxId();
+                Empleado x = introducirEmpleado(idMax);
+                if(x != null)
+                   dao.introEmpleado(x);
+                        
+                do{
+                System.out.println("Quieres introducir otro empleado? (1- Si 0- No)");
+                opcion=teclado.nextInt();
+                teclado.nextLine();
+                if(opcion!=0 && opcion!=1)
+                        System.out.println("Opcion incorrecta");
+                }while(opcion!=0 && opcion!=1);
+            }while(opcion!=0);
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static Empleado introducirEmpleado(int idMax){
+        try{
+            System.out.println("Dime nombre: ");
+            String nombre = teclado.nextLine();
+            System.out.println("Dime categoria: ");
+            String cat = teclado.nextLine();
+            System.out.println("Dime salario: ");
+            Float salario = teclado.nextFloat();
+            teclado.nextLine();
+            System.out.println("Dime Fecha Nac (aaaa-mm-dd): ");
+            String fecha=teclado.nextLine();
+            LocalDate fechaNac = LocalDate.parse(fecha);
+            return new Empleado(++idMax,nombre,salario,fechaNac,cat);
+        }catch(Exception e){
+            System.out.println("Se ha producido un error en la inserción");
+            return null;
+        }
+    }
+}
