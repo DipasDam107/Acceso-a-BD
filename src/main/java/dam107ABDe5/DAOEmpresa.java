ac925e07ee1204fca6917ec427bb82f21c654e46
Daniel Dipas <danieldipas11@hotmail.com>
2020-08-03 14:02:51 +0200

    Ejercicio 5

---
+++ src/main/java/dam107ABDe5/DAOEmpresa.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam107ABDe5;
+
+import dam107ABDe4.*;
+import dam107ABDe3.*;
+import dam107ABDe2.*;
+import dam107ABDe1.*;
+import java.io.IOException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author User
+ */
+public class DAOEmpresa {
+
+    private static DAOEmpresa instance = null;
+    private Connection con = null;
+
+    private DAOEmpresa() throws SQLException, IOException {
+        if (con == null) {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/empresa", "root", "");
+        }
+    }
+
+    public static DAOEmpresa getInstance() throws SQLException, IOException {
+        if (instance == null) {
+            instance = new DAOEmpresa();
+        }
+        return instance;
+    }
+    
+    public int introEmpleado(Empleado emp) throws SQLException{
+        try{
+        String consulta = "INSERT INTO empleado (id,nombre,categoria,salario,fechaNacimiento) VALUES (?,?,?,?,?)";
+        PreparedStatement ps = con.prepareStatement(consulta);
+        ps.setInt(1, emp.getId());
+        ps.setString(2, emp.getNombre());
+        ps.setString(3, emp.getCategoria());
+        ps.setFloat(4, emp.getSalario());
+        ps.setDate(5, java.sql.Date.valueOf(emp.getFechaNac()));
+        return ps.executeUpdate();
+        }catch(SQLException e){
+            System.out.println("Error, no se han introducido filas");
+            return 0;
+        }
+    }
+    
+}
---
+++ src/main/java/dam107ABDe5/Main.java
@@ -0,0 +1,63 @@
+/*
+Realizar un programa que solicite al usuario todos los datos de un empleado y que inserte una
+nueva fila en la tabla Empleado. Después de introducir un empleado, preguntará si se desea
+insertar más, repitiendo el proceso mientras el usuario lo desee.
+ */
+package dam107ABDe5;
+
+import dam107ABDe4.*;
+import java.io.IOException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main {
+    static Scanner teclado;
+    public static void main(String[] args) {
+        teclado = new Scanner(System.in);
+        try {
+            DAOEmpresa dao = DAOEmpresa.getInstance();
+            int opcion=0;
+            do{
+                Empleado x = introducirEmpleado();
+                if(x != null)
+                   dao.introEmpleado(x);
+                        
+                do{
+                System.out.println("Quieres introducir otro empleado? (1- Si 0- No)");
+                opcion=teclado.nextInt();
+                teclado.nextLine();
+                if(opcion!=0 && opcion!=1)
+                        System.out.println("Opcion incorrecta");
+                }while(opcion!=0 && opcion!=1);
+            }while(opcion!=0);
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static Empleado introducirEmpleado(){
+        try{
+            System.out.println("Dime ID: ");
+            int id=teclado.nextInt();
+            teclado.nextLine();
+            System.out.println("Dime nombre: ");
+            String nombre = teclado.nextLine();
+            System.out.println("Dime categoria: ");
+            String cat = teclado.nextLine();
+            System.out.println("Dime salario: ");
+            Float salario = teclado.nextFloat();
+            teclado.nextLine();
+            System.out.println("Dime Fecha Nac (aaaa-mm-dd): ");
+            String fecha=teclado.nextLine();
+            LocalDate fechaNac = LocalDate.parse(fecha);
+            return new Empleado(id,nombre,salario,fechaNac,cat);
+        }catch(Exception e){
+            System.out.println("Se ha producido un error en la inserción");
+            return null;
+        }
+    }
+}
