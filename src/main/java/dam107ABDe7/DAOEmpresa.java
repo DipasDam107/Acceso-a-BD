3f40a9bdba2a722f1efbb2a019e4c36b413b54ea
Daniel Dipas <danieldipas11@hotmail.com>
2020-08-06 21:35:22 +0200

    Ejercicio 7

---
+++ src/main/java/dam107ABDe7/DAOEmpresa.java
@@ -0,0 +1,37 @@
+
+package dam107ABDe7;
+
+import java.io.IOException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+
+public class DAOEmpresa {
+
+    private static DAOEmpresa instance = null;
+    private Connection con = null;
+
+    private DAOEmpresa() throws SQLException, IOException {
+        if (con == null) {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/empresa", "root", "");
+        }
+    }
+
+    public static DAOEmpresa getInstance() throws SQLException, IOException {
+        if (instance == null) {
+            instance = new DAOEmpresa();
+        }
+        return instance;
+    }
+    
+    public int actuSueldos(){
+        try{
+        String consulta = "UPDATE empleado SET salario = 1000 WHERE salario < 1000";
+        PreparedStatement ps = con.prepareStatement(consulta);
+        return ps.executeUpdate();
+        }catch(SQLException e){
+           return -1; 
+//        }
+    }
+}}
---
+++ src/main/java/dam107ABDe7/Main.java
@@ -0,0 +1,31 @@
+/*
+Realizar un programa que actualice el salario de los empleados para que no haya ningún
+empleado que cobre menos de 1000 euros. (Hacerlo mediante UPDATE). El programa
+informará de cuantos empleados se han visto afectados.
+ */
+package dam107ABDe7;
+
+import java.io.IOException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        
+        try {
+            DAOEmpresa dao = DAOEmpresa.getInstance();
+            int actus = dao.actuSueldos();
+            if(actus==-1) System.out.println("Se ha producido un error");
+            else if(actus==0) System.out.println("No ha habido actualizaciones");
+            else System.out.println("Se han producido actualizaciones: "+ actus);
+            
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    
+}
