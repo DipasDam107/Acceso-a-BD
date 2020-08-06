5404d1bb4bcbe80a773c128f74ea55d47a71294a
Daniel Dipas <danieldipas11@hotmail.com>
2020-08-06 22:18:24 +0200

    Ejercicio 9

---
+++ src/main/java/dam107ABDe9/DAOEmpresa.java
@@ -0,0 +1,46 @@
+
+package dam107ABDe9;
+
+import dam107ABDe8.*;
+import dam107ABDe7.*;
+import java.io.IOException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+
+public class DAOEmpresa {
+
+    private static DAOEmpresa instance = null;
+    private Connection con = null;
+
+    private DAOEmpresa() throws SQLException, IOException {
+        if (con == null) {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/empresa", "root", "");
+        }
+    }
+
+    public static DAOEmpresa getInstance() throws SQLException, IOException {
+        if (instance == null) {
+            instance = new DAOEmpresa();
+        }
+        return instance;
+    }
+    
+    public int actuSalario(){
+        int contador=0;
+        try{
+        String consulta = "SELECT id, salario FROM empleado WHERE salario < 1100";
+        PreparedStatement ps = con.prepareStatement(consulta, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+        ResultSet rs = ps.executeQuery();
+        while(rs.next()){
+            rs.updateFloat(2, 1100f);
+            rs.updateRow();
+            contador++;
+        }
+        return contador;
+        }catch(SQLException e){
+           e.printStackTrace();
+           return -1; 
+        }
+}}
---
+++ src/main/java/dam107ABDe9/Main.java
@@ -0,0 +1,31 @@
+/*
+Realizar un programa que actualice el salario de los empleados para que no haya ningún
+empleado que cobre menos de 1100 euros. Hacerlo actualizando un ResultSet. A continuación,
+volver al principio ese ResultSet y mostrar nombre y salario de todos los empleados.
+ */
+package dam107ABDe9;
+
+import java.io.IOException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        
+        try {
+            DAOEmpresa dao = DAOEmpresa.getInstance();
+            int actus = dao.actuSalario();
+            if(actus==-1) System.out.println("Se ha producido un error");
+            else if(actus==0) System.out.println("No ha habido actualizaciones");
+            else System.out.println("Se han producido actualizaciones: "+ actus);
+            
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    
+}
